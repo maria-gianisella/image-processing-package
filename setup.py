1ca395a658d2076f86b8f7571f7beb3bbdc8b364
maria-gianisella <maria.ggianisella@gmail.com>
2024-09-19 14:31:31 -0300

    Atualização do setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image-processing-package",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="maria-gianisella",
+    author_email="maria.ggianisella@gmail.com",
+    description="Python package of image processing",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/maria-gianisella/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
